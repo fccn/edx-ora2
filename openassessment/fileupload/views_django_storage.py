42d96d541b1d2ee1d23689818c48a5ca2581eb3a
Squirrel18 <daniel.quiroga@edunext.co>
2019-03-19 15:15:17 -0500

    Adding support for file extension to django-storage.

--- openassessment/fileupload/views_django_storage.py
+++ openassessment/fileupload/views_django_storage.py
@@ -10,9 +10,9 @@ from .backends.django_storage import Backend
 
 @login_required()
 @require_http_methods(["PUT"])
-def django_storage(request, key):
+def django_storage(request, key, file_ext):
     """
     Upload files using django storage backend.
     """
-    Backend().upload_file(key, request.body)
+    Backend().upload_file(key, request.body, file_ext)
     return HttpResponse()
