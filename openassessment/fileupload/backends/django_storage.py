87af2b7b685b9b1c6e56fe3577aa09d02a18759c
Squirrel18 <daniel.quiroga@edunext.co>
2019-03-19 15:15:17 -0500

    Adding support for file extension to django-storage.

--- openassessment/fileupload/backends/django_storage.py
+++ openassessment/fileupload/backends/django_storage.py
@@ -1,6 +1,13 @@
-""" Django backend for file upload. """
+"""
+Django-storage backend module.
+
+Creates the file alongs with a .meta file that contains info about the file saved.
+"""
 from __future__ import absolute_import
 
+
+import json
+import mimetypes
 import os
 
 from django.core.files.base import ContentFile
@@ -14,11 +21,19 @@ class Backend(BaseBackend):
     """
     Manage openassessment student files uploaded using the default django storage settings.
     """
+    METADATA_FILE_EXTENSION = '.meta'
+
     def get_upload_url(self, key, content_type):
         """
         Return the URL pointing to the ORA2 django storage upload endpoint.
         """
-        return reverse("openassessment-django-storage", kwargs={'key': key})
+        file_extension = get_file_extension(content_type)
+        parameters = {
+            'key': key,
+            'file_ext': file_extension,
+        }
+
+        return reverse("openassessment-django-storage", kwargs=parameters)
 
     def get_download_url(self, key):
         """
@@ -27,16 +42,36 @@ class Backend(BaseBackend):
         Returns None if no file exists at that location.
         """
         path = self._get_file_path(key)
+        metadata_path = '{path}{ext}'.format(path=path, ext=self.METADATA_FILE_EXTENSION)
+
+        # Checks that .meta file exists for this key.
+        if default_storage.exists(metadata_path):
+            metadata_file = default_storage.open(metadata_path).read().decode("utf-8")
+            metadata_dict = get_metadata_dict(metadata_file)
+            path_file_ext = '{path}{ext}'.format(path=path, ext=metadata_dict['ext'])
+
+            if default_storage.exists(path_file_ext):
+                return default_storage.url(path_file_ext)
+
         if default_storage.exists(path):
             return default_storage.url(path)
         return None
 
-    def upload_file(self, key, content):
+    def upload_file(self, key, content, file_ext):
         """
         Upload the given file content to the keyed location.
         """
         path = self._get_file_path(key)
-        saved_path = default_storage.save(path, ContentFile(content))
+        path_file_ext = '{path}{ext}'.format(path=path, ext=file_ext)
+        metadata_dict = {
+            "ext": file_ext
+        }
+        metadata_file = ContentFile(json.dumps(metadata_dict))
+        metadata_path = '{path}{ext}'.format(path=path, ext=self.METADATA_FILE_EXTENSION)
+
+        saved_path = default_storage.save(path_file_ext, ContentFile(content))
+        # Saves the .meta file with the same file key.
+        default_storage.save(metadata_path, metadata_file)
         return saved_path
 
     def remove_file(self, key):
@@ -47,6 +82,20 @@ class Backend(BaseBackend):
         Returns False if the file does not exist, and so was not removed.
         """
         path = self._get_file_path(key)
+        metadata_path = '{path}{ext}'.format(path=path, ext=self.METADATA_FILE_EXTENSION)
+
+        # Checks that .meta file exists for this key.
+        if default_storage.exists(metadata_path):
+            metadata_file = default_storage.open(metadata_path).read()
+            metadata_dict = get_metadata_dict(metadata_file)
+            path_file_ext = '{path}{ext}'.format(path=path, ext=metadata_dict['ext'])
+            # Removes the .meta file too.
+            default_storage.delete(metadata_path)
+
+            if default_storage.exists(path_file_ext):
+                default_storage.delete(path_file_ext)
+                return True
+
         if default_storage.exists(path):
             default_storage.delete(path)
             return True
@@ -69,3 +118,25 @@ class Backend(BaseBackend):
         """
         path = self._get_key_name(self._get_file_name(key))
         return path
+
+
+def get_file_extension(content_type):
+    """
+    Return the correct file extension depending on content_type.
+    """
+    extension = mimetypes.guess_all_extensions(content_type)
+    if extension:
+        return extension[0]
+
+    raise ValueError('Unknown content type file.')
+
+
+def get_metadata_dict(metadata_file):
+    """
+    Return a python dict object from metadata_file.
+    """
+    try:
+        metadata_dict = json.loads(metadata_file)
+        return metadata_dict
+    except ValueError:
+        raise ValueError('Metadata contents could not be loaded correctly.')
